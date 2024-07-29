02227f1c01bd05739561c28c25f4a99dc25143ef
tknightz <hatuyen266@gmail.com>
2024-07-29 22:59:48 +0700

    chore: add barbecue support

---
+++ lua/onedark/barbecue.lua
@@ -0,0 +1,8 @@
+local c = require('onedark.colors')
+local adjust_color = require("onedark.utils").adjust_color
+
+local tkbarbecue = {}
+
+tkbarbecue.normal = { bg = adjust_color(c.bg1, 0.8) }
+
+return tkbarbecue
